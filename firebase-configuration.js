7c4bb300143efdf300c6b36181c03e8d2bd9ee38
DCkGM <upk1719@gmail.com>
2026-08-18 08:40:32 +0530

    Add Firebase configuration for the messaging app

---
+++ firebase-configuration.js
@@ -0,0 +1,23 @@
+const MESS_FIREBASE_CONFIG = {
+
+    apiKey: "IzaSyA5Xt8vTe1Cza4wON_OhQGQ3smATAXbc7I",
+
+    authDomain:
+        "messapp-iitb.firebaseapp.com",
+
+    databaseURL:
+        "http://messapp-iitb-default-rtdb.firebaseio.com/",
+
+    projectId:
+        "messapp-iitb",
+
+    storageBucket:
+        "messapp-iitb.firebasestorage.app",
+
+    messagingSenderId:
+        "554106577587",
+
+    appId:
+        "1:554106577587:web:a03a29247c8424e82586c6"
+
+};
